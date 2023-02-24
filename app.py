369fe87019a5b87e43beba1cb39280f2195b347a
Al Robison <psx66@hotmail.com>
2023-02-23 20:29:31 -0700

    Added ProxyFix

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+
 from flask import Flask
 from views import views
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 app.register_blueprint(views, url_prefix="/")
 
 # Routes
