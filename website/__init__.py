5248f3adfa912069a33931c7318f94b423a53d17
Al Robison <psx66@hotmail.com>
2023-02-28 07:24:38 -0700

    Initial steps for API

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_restful import Api, Resource
 from werkzeug.middleware.proxy_fix import ProxyFix
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data":"HelloWorld"}
+    
+    def put(self):
+        pass
 
 def create_app():
     app = Flask(__name__)
-    #app.config["SECRET_KEY"] = "alfjasghowrbn489h34g498h9*&H34glk%*Yg4"
-    #app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+    app.config["SECRET_KEY"] = "alfjasghowrbn489h34g498h9*&H34glk%*Yg4"
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+    api = Api(app)
+    api.add_resource(HelloWorld, "/helloworld")
 
     from .views import views
     from .auth import auth
@@ -13,3 +23,4 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
     return app
+
