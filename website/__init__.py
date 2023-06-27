18f497c63962703a86e6c83f2d9f5aec27078f53
Al Robison <psx66@hotmail.com>
2023-06-26 21:15:06 -0600

    Updated auth. Created Login and Signup pages.

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_sqlalchemy import SQLAlchemy
 
 """
 class HelloWorld(Resource):
@@ -9,10 +10,14 @@ class HelloWorld(Resource):
     def post(self):
         return "Hello!"
 """
+db = SQLAlchemy()
+DB_NAME = "alrobison.db"
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "alfjasghowrbn489h34g498h9*&H34glk%*Yg4"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     """
