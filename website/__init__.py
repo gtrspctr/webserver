110bc3b439c34c42e1766f89db6e22c2b932314c
Al Robison <psx66@hotmail.com>
2023-09-25 22:53:47 -0600

    Started creating swagger API documentation

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import json
 
 # Create and define database
 db = SQLAlchemy()
@@ -9,8 +12,11 @@ db_name = "database.db"
 
 current_dir = path.dirname(__file__)
 parent_dir = path.dirname(current_dir)
+static_dir = path.join(current_dir, 'static')
 db_dir = path.join(parent_dir, "instance")
 db_path = path.join(db_dir, db_name)
+swagger_dir = path.join(static_dir, "swagger")
+swagger_path = path.join(swagger_dir, "swagger.json")
 
 def create_app():
     app = Flask(__name__)
@@ -22,13 +28,30 @@ def create_app():
 
     # Import and register blueprints
     from .views import views
-
     app.register_blueprint(views, url_prefix="/")
 
     # Import and register database
     from .models import RemoteRequest
     with app.app_context():
         db.create_all()
+
+    # Create Swagger documentation
+    @app.route("/swagger.json")
+    def spec():
+        with open(swagger_path, 'r') as f:
+            return jsonify(json.load(f))
+        
+    
+    SWAGGER_URL = "/apidocs"
+    API_URL = "/swagger.json"
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            "app_name": "Requests API"
+        }
+    )
+    app.register_blueprint(swaggerui_blueprint)
     
     # Import and register API
     from .api import api
