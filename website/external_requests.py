6b7550b0b68a07fafc1272f8b5e4b65bb0733cf3
Al Robison <psx66@hotmail.com>
2024-01-21 17:52:57 -0700

    Update external_requests.py
    Added a "try except" statement because geoiplookup.net went down and it broke my site.

--- website/external_requests.py
+++ website/external_requests.py
@@ -2,18 +2,29 @@ import requests
 
 def lookup_geoip(ip):
     url = "https://api.geoiplookup.net/?query=" + str(ip) + "&json=true"
-    r = requests.get(url)
-    result_dict = {
-        "isp": [],
-        "city": [],
-        "country": []
-    }
-    if "isp" not in r.json():
+    try:
+        r = requests.get(url)
+        result_dict = {
+            "isp": [],
+            "city": [],
+            "country": []
+        }
+        if "isp" not in r.json():
+            result_dict["isp"] = "N/A"
+            result_dict["city"] = "N/A"
+            result_dict["country"] = "N/A"
+        else:
+            result_dict["isp"] = r.json()["isp"]
+            result_dict["city"] = r.json()["city"]
+            result_dict["country"] = r.json()["countrycode"]
+        return result_dict
+    except:
+        result_dict = {
+            "isp": [],
+            "city": [],
+            "country": []
+        }
         result_dict["isp"] = "N/A"
         result_dict["city"] = "N/A"
         result_dict["country"] = "N/A"
-    else:
-        result_dict["isp"] = r.json()["isp"]
-        result_dict["city"] = r.json()["city"]
-        result_dict["country"] = r.json()["countrycode"]
-    return result_dict
+        return result_dict
